8ab90930fad6e1a8d220af6d8b4c21511c030890
Timothy Derycker <timothyderycker@gmail.com>
2019-09-17 15:52:50 +0200

    Add new models

---
+++ BookService.WebAPI/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BookService.WebAPI.Models {
+    public class Book {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        [Display(Name = "#")]
+        public int NumberOfPages { get; set; }
+        public Author Author { get; set; }
+        public Publisher Publisher { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
